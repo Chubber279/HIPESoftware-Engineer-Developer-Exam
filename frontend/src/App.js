3d2886a7a2a69c8d91f345965ad115bc302f3311
Lorenzo Ocampo <lorenzo.ocampo279@gmail.com>
2023-09-15 19:20:02 +0800

    Removed Event Buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import { Events } from "./Components/Events";
-import { EventButtons } from "./Components/EventButtons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Events />
-        <EventButtons />
       </header>
     </div>
   );
