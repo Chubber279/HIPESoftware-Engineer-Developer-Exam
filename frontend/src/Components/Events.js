df5fe28983f51442ea622ea4e86a0d1c73bd26b1
Lorenzo Ocampo <lorenzo.ocampo279@gmail.com>
2023-09-15 19:20:43 +0800

    Added timestamp

--- frontend/src/Components/Events.js
+++ frontend/src/Components/Events.js
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Container } from "react-bootstrap";
+import { EventButtons } from "./EventButtons";
 
 export const Events = () => {
   const [events, setEvents] = useState([]);
+  const [shouldFetch, setShouldFetch] = useState(true);
+
+  const convertDate = (timestamp) =>
+    new Date(timestamp * 1000).toLocaleString();
 
   useEffect(() => {
-    axios
-      .get("http://localhost:8080/events")
-      .then((response) => {
-        setEvents(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching events", error);
-      });
-  }, []);
-
+    // Only fetch events if shouldFetch is true
+    if (shouldFetch) {
+      axios
+        .get("http://localhost:8080/events")
+        .then((response) => {
+          setEvents(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching events", error);
+        })
+        .finally(() => {
+          setShouldFetch(false);
+        });
+    }
+  }, [shouldFetch]);
   return (
     <>
       <Container>
@@ -23,10 +33,12 @@ export const Events = () => {
         <ul>
           {events.map((event) => (
             <li key={event.id}>
-              {event.name} - {event.description}
+              {event.id} - {event.name} - {event.description} -{" "}
+              {convertDate(event.start_time)} - {convertDate(event.end_time)}
             </li>
           ))}
         </ul>
+        <EventButtons setShouldFetch={setShouldFetch} />
       </Container>
     </>
   );
